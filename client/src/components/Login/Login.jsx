9fb573c49ecf0b1de61b23911e66116259dd711f
Ravi Ranjan Kumar <ravirkumar2422@gmail.com>
2024-01-31 13:55:11 +0530

    complete the front-end part

---
+++ client/src/components/Login/Login.jsx
@@ -0,0 +1,88 @@
+import React, { useState } from 'react';
+import {
+  Box,
+  Container,
+  FormControl,
+  FormLabel,
+  Input,
+  Button,
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogContent,
+  AlertDialogOverlay,
+} from '@chakra-ui/react';
+
+const LoginPage = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+    setIsOpen(true);
+    setTimeout(() => {
+      setIsOpen(false);
+      window.location.href = '/dashboard';
+    }, 2000);
+  };
+
+  return (
+    <Box
+      bg="linear-gradient(to bottom right, #6F3BA9, #9068BE)"
+      minH="100vh"
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Container
+        p={8}
+        borderWidth={1}
+        borderRadius="lg"
+        boxShadow="lg"
+        borderColor="white" 
+        textAlign="center"
+      >
+        <h1 style={{ color: 'white' }}>Welcome to Admin !!!</h1>
+        <form>
+          <FormControl>
+            <FormLabel style={{ color: 'white' }}>Admin Email</FormLabel>
+            <Input
+              type="text"
+              placeholder="Enter your username"
+              value="admin@gmail.com"
+              borderColor="white" 
+            />
+          </FormControl>
+          <FormControl mt={4}>
+            <FormLabel style={{ color: 'white' }}>Password</FormLabel>
+            <Input
+              type="password"
+              placeholder="Enter your password"
+              value="admin" 
+              borderColor="white"
+            />
+          </FormControl>
+          <Button colorScheme="green" mt={6} w="100%" onClick={handleLogin}>
+            Login
+          </Button>
+          {/* AlertDialog */}
+          <AlertDialog isOpen={isOpen} leastDestructiveRef={undefined}>
+            <AlertDialogOverlay>
+              <AlertDialogContent bg="purple.800" color="white">
+                <AlertDialogHeader>Welcome Admin !!!</AlertDialogHeader>
+                <AlertDialogBody>
+                  Redirecting to the dashboard page...
+                </AlertDialogBody>
+                <AlertDialogFooter>
+                  <Button onClick={() => setIsOpen(false)}>Cancel</Button>
+                </AlertDialogFooter>
+              </AlertDialogContent>
+            </AlertDialogOverlay>
+          </AlertDialog>
+        </form>
+      </Container>
+    </Box>
+  );
+};
+
+export default LoginPage;
